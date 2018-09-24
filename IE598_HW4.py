578e844cc29c20d2b3b628cb4edcc01ae97fdc56
Jiayonguiuc <42919498+Jiayonguiuc@users.noreply.github.com>
2018-09-23 23:17:01 -0500

    Add files via upload

---
+++ IE598_HW4.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 23 16:35:12 2018
+
+@author: jiayonghuang
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import RANSACRegressor
+from sklearn.model_selection import train_test_split
+import scipy as sp
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+df = pd.read_csv('https://raw.githubusercontent.com/rasbt/'
+                 'python-machine-learning-book-2nd-edition'
+                 '/master/code/ch10/housing.data.txt',
+                 header=None,
+                 sep='\s+')
+
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 
+              'NOX', 'RM', 'AGE', 'DIS', 'RAD', 
+              'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+df.head()
+
+cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
+sns.pairplot(df[cols], size=2.5)
+plt.tight_layout()
+# plt.savefig('images/10_03.png', dpi=300)
+plt.show()
+
+cm = np.corrcoef(df[cols].values.T)
+#sns.set(font_scale=1.5)
+hm = sns.heatmap(cm,
+                 cbar=True,
+                 annot=True,
+                 square=True,
+                 fmt='.2f',
+                 annot_kws={'size': 15},
+                 yticklabels=cols,
+                 xticklabels=cols)
+
+plt.tight_layout()
+# plt.savefig('images/10_04.png', dpi=300)
+plt.show()
+
+X = df.iloc[:, :-1].values
+y = df['MEDV'].values
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+print( X_train.shape, y_train.shape)
+slr = LinearRegression()
+slr.fit(X_train, y_train)
+y_train_pred = slr.predict(X_train)
+y_test_pred = slr.predict(X_test)
+
+plt.scatter(y_train_pred,  y_train_pred - y_train,
+            c='steelblue', marker='o', edgecolor='white',
+            label='Training data')
+plt.scatter(y_test_pred,  y_test_pred - y_test,
+            c='limegreen', marker='s', edgecolor='white',
+            label='Test data')
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, color='black', lw=2)
+plt.xlim([-10, 50])
+plt.tight_layout()
+plt.show()
+print('Slope: %.3f' % slr.coef_[0])
+print('Intercept: %.3f' % slr.intercept_)
+print('MSE train: %.3f, test: %.3f' % (
+        mean_squared_error(y_train, y_train_pred),
+        mean_squared_error(y_test, y_test_pred)))
+print('R^2 train: %.3f, test: %.3f' % (
+        r2_score(y_train, y_train_pred),
+        r2_score(y_test, y_test_pred)))
+
+alpha_possible=[0.05, 0.1, 0.45, 0.8, 1.0]
+for n in alpha_possible:
+    print ('\n alpha= ',n)
+    
+#Ridge regression
+    print('\nRidge')
+    ridge=Ridge(alpha=n)
+    ridge.fit(X_train, y_train)
+    X_train_pred = ridge.predict(X_train)
+    y_test_pred = ridge.predict(X_test)
+    
+    plt.scatter(y_train_pred, y_train_pred - y_train,
+            c='steelblue', marker='o', edgecolor='white',
+            label='Training data')
+    plt.scatter(y_test_pred,  y_test_pred - y_test,
+            c='limegreen', marker='s', edgecolor='white',
+            label='Test data')
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residuals')
+    plt.legend(loc='upper left')
+    plt.hlines(y=0, xmin=-10, xmax=50, color='black', lw=2)
+    plt.xlim([-10, 50])
+    plt.tight_layout()
+    plt.show()
+    print('MSE train: %.3f, test: %.3f' % (
+        mean_squared_error(y_train, y_train_pred),
+        mean_squared_error(y_test, y_test_pred)))
+    print('R^2 train: %.3f, test: %.3f' % (
+        r2_score(y_train, y_train_pred),
+        r2_score(y_test, y_test_pred)))
+    
+
+
+#Lasso regression
+    print('\nLasso')
+    lasso=Lasso(alpha=n)
+    lasso.fit(X_train, y_train)
+    X_train_pred = lasso.predict(X_train)
+    y_test_pred = lasso.predict(X_test)
+    
+    plt.scatter(y_train_pred, y_train_pred - y_train,
+            c='steelblue', marker='o', edgecolor='white',
+            label='Training data')
+    plt.scatter(y_test_pred,  y_test_pred - y_test,
+            c='limegreen', marker='s', edgecolor='white',
+            label='Test data')
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residuals')
+    plt.legend(loc='upper left')
+    plt.hlines(y=0, xmin=-10, xmax=50, color='black', lw=2)
+    plt.xlim([-10, 50])
+    plt.tight_layout()
+    plt.show()
+    print('Slope: %.3f' % slr.coef_[0])
+    print('Intercept: %.3f' % slr.intercept_)
+    print('MSE train: %.3f, test: %.3f' % (
+        mean_squared_error(y_train, y_train_pred),
+        mean_squared_error(y_test, y_test_pred)))
+    print('R^2 train: %.3f, test: %.3f' % (
+        r2_score(y_train, y_train_pred),
+        r2_score(y_test, y_test_pred)))
+    
+#Elastic Net Regression
+l1_ratio_possible=[0.05, 0.1, 0.45, 0.8, 1.0]
+for m in l1_ratio_possible:
+    print ('\n Elastic Net')
+    print ('\n alpha= 1.0')
+    print('\nelanet= ', m)
+    elanet=ElasticNet(alpha = 1.0, l1_ratio = m)
+    elanet.fit(X_train, y_train)
+    X_train_pred = elanet.predict(X_train)
+    y_test_pred = elanet.predict(X_test)
+        
+    plt.scatter(y_train_pred, y_train_pred - y_train,
+                c='steelblue', marker='o', edgecolor='white',
+                label='Training data')
+    plt.scatter(y_test_pred,  y_test_pred - y_test,
+                c='limegreen', marker='s', edgecolor='white',
+                label='Test data')
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residuals')
+    plt.legend(loc='upper left')
+    plt.hlines(y=0, xmin=-10, xmax=50, color='black', lw=2)
+    plt.xlim([-10, 50])
+    plt.tight_layout()
+    plt.show()
+    print('MSE train: %.3f, test: %.3f' % (
+        mean_squared_error(y_train, y_train_pred),
+        mean_squared_error(y_test, y_test_pred)))
+    print('R^2 train: %.3f, test: %.3f' % (
+        r2_score(y_train, y_train_pred),
+        r2_score(y_test, y_test_pred)))
+    
+ # Summary
